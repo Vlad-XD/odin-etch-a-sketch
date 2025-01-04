cc7369359fcf16fdbf3f5d5b29f12ed2be353d4b
Vlad-XD <192461579+Vlad-XD@users.noreply.github.com>
2025-01-04 13:52:42 -0800

    Implement "hover" effect on grid to create pixel trail

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
+// variable initializations
+let PIXEL_WIDTH = "5px";
+let PIXEL_HEIGHT = PIXEL_WIDTH;
+
 // find our grid container div
 const flexContainer = document.querySelector(".grid-container");
 
 // use loop to create 16 grids
 for(let i = 0; i < 16; i++) {
+  // create new square grid
   let newGrid = document.createElement("div");
   newGrid.className = "grid-square";
-  flexContainer.appendChild(newGrid);
-  print(i);
-}
 
-function print(text){
-  console.log(text);
+  // add hover listener to grid
+  newGrid.addEventListener("mousemove", (e) => {
+
+    // create a "pixel" box and add to the container where mouse hovers
+    let pixel = document.createElement("div");
+    pixel.className = "pixel";
+    pixel.style.minWidth = PIXEL_WIDTH;
+    pixel.style.minHeight = PIXEL_HEIGHT;
+    pixel.style.backgroundColor = "black";
+    pixel.style.position = "absolute";
+    pixel.style.left = `${e.clientX}px`;
+    pixel.style.top = `${e.clientY}px`;
+    e.currentTarget.appendChild(pixel);
+  })
+
+  // append square grid
+  flexContainer.appendChild(newGrid);
 }
