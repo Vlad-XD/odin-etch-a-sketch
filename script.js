fffd8d5af0c75f2bc8186040ff8e4978bbe8a9b5
Vlad-XD <192461579+Vlad-XD@users.noreply.github.com>
2025-01-04 19:04:29 -0800

    Upload changes made to the calculating box size method
    Saving just in case. Planning on going back to an earlier point and
    attempting a simpler approach...

--- script.js
+++ script.js
@@ -6,12 +6,20 @@ const INITIAL_PROMPT_MESSAGE = "Input a new grid size: ";
 const INVALID_PROMPT_MESSAGE = "Not a valid input!\nEnter new input:";
 const MAX_LIMIT_PROMPT_MESSAGE = `Maximum grid size is ${MAX_USER_INPUT}.\nEnter new input:`;
 const MIN_LIMIT_PROMPT_MESSAGE = `Minimum grid size is ${MIN_USER_INPUT}.\nEnter new input:`;
+const GRID_BOX_DIMENSIONS = 1000;
+const GRID_BOX_PADDING = 50;
+
+// set dimensions and padding of grid box containing grid container/grids
+const gridBox = document.querySelector(".grid-box");
+gridBox.style.width = `${GRID_BOX_DIMENSIONS}px`;
+gridBox.style.height = `${GRID_BOX_DIMENSIONS}px`;
+gridBox.style.padding = `${GRID_BOX_PADDING}px`;
 
 // find our grid container div
 const flexContainer = document.querySelector(".grid-container");
 
 // create initial grid
-createGrid(DEFAULT_GRID_SIZE, flexContainer);
+createGrid(DEFAULT_GRID_SIZE, flexContainer, GRID_BOX_DIMENSIONS, GRID_BOX_PADDING);
 
 // when input button is pressed, prompt user for new grid size
 const inputButton = document.querySelector("#input-button");
@@ -48,7 +56,7 @@ inputButton.addEventListener("click", () => {
       validInputEntered = true;
       // delete old grid and make new grid
       destroyGrid(flexContainer);
-      createGrid(userInputInt, flexContainer);
+      createGrid(userInputInt, flexContainer, GRID_BOX_DIMENSIONS, GRID_BOX_PADDING);
     }
     
   } while(!validInputEntered);
@@ -58,17 +66,23 @@ inputButton.addEventListener("click", () => {
 
 /*
   createGrid: adds drawables grids to the pass gridContainer element.
-              Number of grids based on passed gridSize argument.
-  Parameters: int gridSize, <div> gridContainer
+              Number of grids based on passed gridSize argument. Utilizes
+              passed gridBox properties to size drawable grids.
+  Parameters: int gridSize, <div> gridContainer, <div> gridBox
   Return value: N/A
 */
 
-function createGrid(gridSize, gridContainer) {
+function createGrid(gridSize, gridContainer, gridBoxDimension, gridBoxPadding) {
   // use loop to create grids
-  for(let i = 0; i < gridSize; i++) {
+  for(let i = 0; i < gridSize**2; i++) {
     // create new square grid
     let newGrid = document.createElement("div");
     newGrid.className = "grid-square";
+
+    // calculate square grid dimensions
+    let gridDimension = calcGridDim(gridSize, gridBoxDimension, gridBoxPadding);
+    newGrid.style.width = `${gridDimension}px`;
+    newGrid.style.height = `${gridDimension}px`;
 
     // add hover listener to grid
     newGrid.addEventListener("mousemove", (e) => {
@@ -92,4 +106,19 @@ function destroyGrid(gridContainer) {
   gridContainerChildren.forEach((grid) => {
     gridContainer.removeChild(grid);
   })
+}
+
+/*
+  calcGridDim: given the number of grids and the dimension/padding of the
+               box containing the grids, returns the grid dimension to make
+               the squares fit evenly in the grid.
+  Parameters: int gridSize, int boxDimension, int boxPadding
+  Return value: float gridDimension
+*/
+
+function calcGridDim(gridSize, boxDimension, boxPadding) {
+  // variable initializations
+  let gridDimension = (boxDimension)/gridSize;
+  return gridDimension;
+  
 }
