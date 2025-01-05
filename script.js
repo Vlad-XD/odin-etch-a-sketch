bfafb823b15a392cc5b086e791fccb90a03428ff
Vlad-XD <192461579+Vlad-XD@users.noreply.github.com>
2025-01-04 16:15:20 -0800

    Change behavior of program to match expected output
    Made a mistake and implemented the program incorrectly (leading to some
    headaches). Reviewed some of the other student examples and updated
    program to match expected behavior: each grid itself will change
    background colors permanently once hovered over. Not the pixels within
    the grid.

--- script.js
+++ script.js
@@ -6,28 +6,14 @@ let PIXEL_HEIGHT = PIXEL_WIDTH;
 const flexContainer = document.querySelector(".grid-container");
 
 // use loop to create 16 grids
-for(let i = 0; i < 16; i++) {
+for(let i = 0; i < 64; i++) {
   // create new square grid
   let newGrid = document.createElement("div");
   newGrid.className = "grid-square";
 
   // add hover listener to grid
   newGrid.addEventListener("mousemove", (e) => {
-
-    // create a "pixel" box and add to the container where mouse hovers
-    let pixel = document.createElement("div");
-    pixel.className = "pixel";
-    pixel.style.minWidth = PIXEL_WIDTH;
-    pixel.style.minHeight = PIXEL_HEIGHT;
-    pixel.style.backgroundColor = "black";
-    pixel.style.position = "absolute";
-    //without setting pointerEvents to none, pixels can cause offsetX/Y to be incorrect
-    pixel.style.pointerEvents = "none"; 
-    let rect = e.currentTarget.getBoundingClientRect();
-    pixel.style.left = `${e.offsetX}px`;
-    pixel.style.top = `${e.offsetY}px`;
-    e.currentTarget.appendChild(pixel);
-    // console.log(`Target: ${e.currentTarget.className} X: Y:`);
+    e.currentTarget.style.backgroundColor = "black";
   })
 
   // append square grid
